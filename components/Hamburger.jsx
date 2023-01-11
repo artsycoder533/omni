709ed5dde521da1adff1c89701609e7204757508
Natasha <njohnson533@gmail.com>
2023-01-10 21:30:22 -0500

    chore: fix hamburger button

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -2,34 +2,35 @@ import React from "react";
 
 function Hamburger({ open, setOpen }) {
   return (
-    <button
-      aria-label="menu"
-      aria-haspopup="menu"
-      tabIndex="0"
-      onClick={() => setOpen(!open)}
-      className="w-11 h-11 flex flex-col justify-center relative md:hidden">
-      <span
-        className={
-          "w-full h-[4px] bg-black rounded-lg transition-all ease-in duration-300 absolute " +
-          (open ? "origin-center rotate-[400deg]" : "translate-y-[-14px]")
-        }
-      />
-      <span
-        className={
-          "w-full h-[4px] bg-black rounded-lg transition-opacity " +
-          (open ? "opacity-0" : "opacity-100")
-        }
-      />
-      <span
-        className={
-          "w-full h-[4px] bg-black rounded-lg transition-all ease-in duration-300 absolute " +
-          (open ? "origin-center -rotate-45 bg-white" : "translate-y-[14px]")
-        }
-      />
-    </button>
+    <>
+      <button
+        aria-label="menu"
+        aria-haspopup="menu"
+        tabIndex="0"
+        onClick={() => setOpen(!open)}
+        className="w-14 h-12 flex flex-col justify-center items-center relative md:hidden border-none">
+        <span
+          className={
+            "w-full h-[6px] bg-[#1A8C91] rounded-lg transition-all ease-in duration-300 absolute " +
+            (open ? "origin-center rotate-[400deg]" : "translate-y-[-16px]")
+          }
+        />
+        <span
+          className={`w-full h-[6px] bg-black rounded-lg transition-opacity absolute ${
+            open ? "opacity-0" : "opacity-100"
+          }`}
+        />
+        <span
+          className={
+            "w-full h-[6px] bg-black rounded-lg transition-all ease-in duration-300 absolute " +
+            (open
+              ? "origin-center -rotate-45 bg-black"
+              : "translate-y-[16px]")
+          }
+        />
+      </button>
+    </>
   );
 }
 
 export default Hamburger;
-//translate-y-[-10px]
-//translate-y-[10px]
