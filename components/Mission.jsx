e35d8ce3b5138c0721d60aad6a14f1862bc11a7d
Natasha <njohnson533@gmail.com>
2023-01-12 18:16:11 -0500

    chore: fix mission section padding

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Mission() {
   return (
     <div className="bg-[#1A8C91] text-white" id="our-mission">
-      <blockquote className="max-w-[1400px] w-[70%] mx-auto py-24 px-16 text-3xl">
+      <blockquote className="max-w-[1400px] w-[70%] mx-auto py-16 md:px-16 text-3xl text-center leading-relaxed">
         "Our mission is to heal clients from the negative effects of
         dysfunctional or ineffective thought patterns and behaviors by providing
-        optimal, yet affordable services to the communities of southside VA and
+        optimal, yet affordable services to the communities of Southside VA and
         beyond."
       </blockquote>
     </div>
