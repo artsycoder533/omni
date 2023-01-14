a153961a3e4439d94fc5e418b1a5f9dcf1ccef90
Natasha <njohnson533@gmail.com>
2023-01-14 08:59:04 -0500

    fix mission mobile style

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Mission() {
   return (
     <div className="bg-[#1A8C91] text-white" id="our-mission">
-      <blockquote className="max-w-[1400px] w-[70%] mx-auto py-16 md:px-16 text-3xl text-center leading-relaxed">
+      <blockquote className="max-w-[1400px] w-[90vw] md:w-[70%] mx-auto py-16 md:px-16 text-3xl text-center leading-relaxed">
         "Our mission is to heal clients from the negative effects of
         dysfunctional or ineffective thought patterns and behaviors by providing
         optimal, yet affordable services to the communities of Southside VA and
